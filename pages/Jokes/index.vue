e6e8a187e44c339e05699b7edb7f38b259d890e7
faisalindrianto <wwf051099@gmail.com>
2020-05-23 13:51:43 +0700

    bind data

--- pages/Jokes/index.vue
+++ pages/Jokes/index.vue
@@ -1,9 +1,36 @@
 <template>
-  <div>jokes</div>
+  <div>
+    <Joke v-for="joke in jokes" :key="joke.id" :id="joke.id" :joke="joke.joke" />
+  </div>
 </template>
 
 <script>
+import axios from "axios";
+import Joke from "../../components/Joke";
+
 export default {
+  components: {
+    Joke
+  },
+  data() {
+    return {
+      jokes: []
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.get("https://icanhazdadjoke.com/search", config);
+      this.jokes = res.data.results;
+    } catch (error) {
+      console.log(error);
+    }
+  },
   head() {
     return {
       title: "Dad Jokes",
