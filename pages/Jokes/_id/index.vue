ecb43255e8b24d1ebd23564ef8755ed21517ec24
faisalindrianto <wwf051099@gmail.com>
2020-05-23 14:20:52 +0700

    view single joke

---
+++ pages/Jokes/_id/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <nuxt-link to="/jokes">Back to jokes</nuxt-link>
+    <h4>{{ joke }}</h4>
+    <hr />
+    <small>Joke ID : {{ $route.params.id }}</small>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      joke: {}
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.get(
+        `https://icanhazdadjoke.com/j/${this.$route.params.id}`,
+        config
+      );
+
+      this.joke = res.data.joke;
+      console.log(this.joke);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  head() {
+    return {
+      title: this.joke,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny dad jokes"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
